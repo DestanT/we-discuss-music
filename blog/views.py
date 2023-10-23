2776a57e7701a894fa8e63a7a0db27fbcf11e16b
Destan <destantezel@googlemail.com>
2023-10-23 19:19:07 +0100

    Add SeasonList view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Season
 
 
 def homepage(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
+
+
+class SeasonList(ListView):
+    model = Season
+    queryset = Season.objects.all().order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 4
