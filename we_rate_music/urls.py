459a02d5c0fc9827f3d49645209f9df9c5d32024
Destan <destantezel@googlemail.com>
2023-10-23 19:18:26 +0100

    Change the homepage to display SeasonList view

--- we_rate_music/urls.py
+++ we_rate_music/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import homepage
+from blog.views import SeasonList
 
 urlpatterns = [
-    path('', homepage, name='homepage'),
+    path('', SeasonList.as_view(), name='homepage'),
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ]
