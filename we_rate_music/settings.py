e8d67dcf6a706d8ff5a0deca7ae569f00777cf32
Destan <destantezel@googlemail.com>
2023-10-23 16:25:45 +0100

    Change allowed hosts in settings.py

--- we_rate_music/settings.py
+++ we_rate_music/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-destant-weratemusicsimp-6gepds050v7.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = [os.getenv('LOCAL_HOST', '')]
 
 
 # Application definition
