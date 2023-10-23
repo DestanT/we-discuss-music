ede77d97664f0f9d21cbf875dcbf937e79b3e244
Destan <destantezel@googlemail.com>
2023-10-23 19:48:06 +0100

    Add latest season to context data of SeasonList view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Season
@@ -12,3 +13,8 @@ class SeasonList(ListView):
     queryset = Season.objects.all().order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_season'] = Season.objects.first()
+        return context
