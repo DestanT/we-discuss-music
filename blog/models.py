85676736f63015501625d3930fe41a0b6e00d9e6
Destan <destantezel@googlemail.com>
2023-10-23 18:31:50 +0100

    Create Season model and make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Season(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100)
+    description = models.TextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='seasons')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
