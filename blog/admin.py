9bda4d919c7db683acdb4da1d37c5e2867849109
Destan <destantezel@googlemail.com>
2023-10-23 18:33:33 +0100

    Register Season model to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('created_on', 'author', 'title')
+    prepopulated_fields = {'slug': ('title',)}
