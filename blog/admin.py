2bc5e82ccde93b6aa989d5ae70504c932bcef929
Destan <destantezel@googlemail.com>
2023-10-24 01:24:24 +0100

    Register comment model to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -6,3 +6,15 @@ from .models import *
 class SeasonAdmin(admin.ModelAdmin):
     list_display = ('created_on', 'author', 'title')
     prepopulated_fields = {'slug': ('title',)}
+
+
+# Credit: CI 'Django Blog' walkthrough project
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'season', 'created_on', 'body', 'approved')
+    list_filter = ('user', 'season', 'created_on', 'approved')
+    search_fields = ('user', 'season', 'body')
+    actions = ['approve_comment']
+
+    def approve_comment(self, request, queryset):
+        queryset.update(approved=True)
