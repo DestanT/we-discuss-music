2b7a65e55c3416a32d4c8ec444d7d00818c41c14
Destan <destantezel@googlemail.com>
2023-10-26 13:48:12 +0100

    Heroku deployment commit continued

--- we_rate_music/settings.py
+++ we_rate_music/settings.py
@@ -13,9 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from dotenv import load_dotenv
-
-load_dotenv()
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +24,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('LOCAL_HOST', ''), 'we-rate-music-simplified-c7886543742e.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('LOCAL_HOST'), 'we-rate-music-simplified-c7886543742e.herokuapp.com']
 
 
 # Application definition
@@ -109,7 +108,7 @@ WSGI_APPLICATION = 'we_rate_music.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DATABASE_URL', ''))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
