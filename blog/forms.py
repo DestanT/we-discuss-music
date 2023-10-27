59acf99c94e933b79dcea14f7c9597dd1becafdc
Destan <destantezel@googlemail.com>
2023-10-27 14:51:31 +0100

    Create CommentReplyForm

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
-from .models import Comment
+from .models import Comment, CommentReply
 from django import forms
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
+        fields = ('body',)
+
+
+class CommentReplyForm(forms.ModelForm):
+    class Meta:
+        model = CommentReply
         fields = ('body',)
