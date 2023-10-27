37834fc55ca5ea1da5f40e76d07d24250f673d3c
Destan <destantezel@googlemail.com>
2023-10-27 16:13:36 +0100

    Change season_details to season_detail for simplicity

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<slug:slug>/", views.SeasonDetailView.as_view(), name="season_details"),
+    path("<slug:slug>/", views.SeasonDetailView.as_view(), name="season_detail"),
 ]
