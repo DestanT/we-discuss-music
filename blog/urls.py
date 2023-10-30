e069aba5de1068fcc17ecafb2c71842d56061a07
Destan <destantezel@googlemail.com>
2023-10-30 15:52:07 +0000

    Requiree login before being able to comment or reply to a comment

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path("create/", views.SeasonCreateView.as_view(), name="create_season"),
     path("<slug:slug>/", views.SeasonDetailView.as_view(), name="season_detail"),
-    path("<slug:slug>/comment", views.CommentCreateView.as_view(), name="season_comment"),
-    path("<slug:slug>/<int:id>/reply", views.ReplyCreateView.as_view(), name="comment_reply"),
+    path("<slug:slug>/comment", login_required(views.CommentCreateView.as_view()), name="season_comment"),
+    path("<slug:slug>/<int:id>/reply", login_required(views.ReplyCreateView.as_view()), name="comment_reply"),
 ]
