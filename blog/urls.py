bebcfc9589caaa634ade056162cc276b7d39d5a1
Destan <destantezel@googlemail.com>
2023-11-03 17:59:53 +0000

    Change url path for a Spotify search to follow the season-slug link

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("create/", views.SeasonCreateView.as_view(), name="create_season"),
     path("<slug:slug>/", views.SeasonDetailView.as_view(), name="season_detail"),
+    path("<slug:slug>/spotify-search", views.SpotifyApiView.as_view(), name='spotify_search'),
     path("<slug:slug>/comment", login_required(views.CommentCreateView.as_view()), name="season_comment"),
     path("<slug:slug>/<int:id>/reply", login_required(views.ReplyCreateView.as_view()), name="comment_reply"),
     path("<slug:slug>/<int:pk>/update", login_required(views.CommentUpdateView.as_view()), name="update_comment"),
