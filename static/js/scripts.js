3daaa19f9d58f76fc4119711f512ace9d671c6d3
Destan <destantezel@googlemail.com>
2023-11-08 14:49:01 +0000

    Add if statement to Spotify iFrame embed to only execute if it exists in the DOM

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,20 +1,23 @@
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
     const element = document.getElementById('embed-iframe');
-    // Get parent div of 'element' - for d-none toggle
-    const parent = element.parentElement;
 
-    const options = {
-        width: '100%',
-    };
-
-    const callback = (EmbedController) => {
-        document.querySelectorAll('.spotify-iframe').forEach(
-            playlist => {
-                playlist.addEventListener('click', () => {
-                    parent.classList.remove('d-none');
-                    EmbedController.loadUri(playlist.dataset.spotifyId)
-                });
-            })
-    };
-    IFrameAPI.createController(element, options, callback);
+    if (element) {
+        // Get parent div of 'element' - for d-none toggle
+        const parent = element.parentElement;
+        
+        const options = {
+            width: '100%',
+        };
+        
+        const callback = (EmbedController) => {
+            document.querySelectorAll('.spotify-iframe').forEach(
+                playlist => {
+                    playlist.addEventListener('click', () => {
+                        parent.classList.remove('d-none');
+                        EmbedController.loadUri(playlist.dataset.spotifyId)
+                    });
+                })
+            };
+            IFrameAPI.createController(element, options, callback);
+        }
 };
