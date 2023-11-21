a940ee6381141fb9c7d943eb4d570c000d3e76eb
Destan <destantezel@googlemail.com>
2023-11-21 19:13:19 +0000

    Change deprecated assertEquals method to assertEqual - without the 's'

--- blog/tests/test_urls.py
+++ blog/tests/test_urls.py
@@ -10,39 +10,39 @@ class TestUrls(SimpleTestCase):
 
     def test_homepage_url_resolves(self):
         url = reverse('homepage')
-        self.assertEquals(resolve(url).func.view_class, SeasonListView)
+        self.assertEqual(resolve(url).func.view_class, SeasonListView)
 
     def test_create_season_url_resolves(self):
         url = reverse('create_season')
-        self.assertEquals(resolve(url).func.view_class, SeasonCreateView)
+        self.assertEqual(resolve(url).func.view_class, SeasonCreateView)
 
     def test_season_detail_url_resolves(self):
         url = reverse('season_detail', args=['test-slug'])
-        self.assertEquals(resolve(url).func.view_class, SeasonDetailView)
+        self.assertEqual(resolve(url).func.view_class, SeasonDetailView)
 
     def test_spotify_search_url_resolves(self):
         url = reverse('spotify_search', args=['test-slug'])
-        self.assertEquals(resolve(url).func.view_class, SpotifyApiView)
+        self.assertEqual(resolve(url).func.view_class, SpotifyApiView)
 
     def test_add_playlist_url_resolves(self):
         url = reverse('add_playlist', args=['test-slug'])
-        self.assertEquals(resolve(url).func.view_class, AddPlaylistView)
+        self.assertEqual(resolve(url).func.view_class, AddPlaylistView)
 
     def test_season_comment_url_resolves(self):
         url = reverse('season_comment', args=['test-slug'])
-        self.assertEquals(resolve(url).func.view_class, CommentCreateView)
+        self.assertEqual(resolve(url).func.view_class, CommentCreateView)
 
     def test_comment_reply_url_resolves(self):
         url = reverse('comment_reply', args=['test-slug', '1'])
-        self.assertEquals(resolve(url).func.view_class, ReplyCreateView)
+        self.assertEqual(resolve(url).func.view_class, ReplyCreateView)
 
     def test_update_comment_url_resolves(self):
         url = reverse('update_comment', args=['test-slug', '1'])
-        self.assertEquals(resolve(url).func.view_class, CommentUpdateView)
+        self.assertEqual(resolve(url).func.view_class, CommentUpdateView)
 
     def test_delete_comment_url_resolves(self):
         url = reverse('delete_comment', args=['test-slug', '1'])
-        self.assertEquals(resolve(url).func.view_class, CommentDeleteView)
+        self.assertEqual(resolve(url).func.view_class, CommentDeleteView)
 
 
 class TestLoginRequired(TestCase):
