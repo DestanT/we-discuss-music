adde9bfe733916b7a1be0d29823ea8ebec972cc7
Destan <destantezel@googlemail.com>
2023-11-23 14:49:13 +0000

    Add update_season url path

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("create/", views.SeasonCreateView.as_view(), name="create_season"),
     path("<slug:slug>/", views.SeasonDetailView.as_view(), name="season_detail"),
+    path("<slug:slug>/update", views.SeasonUpdateView.as_view(), name="update_season"),
     path("<slug:slug>/spotify-search", views.SpotifyApiView.as_view(), name='spotify_search'),
     path("<slug:slug>/add-playlist", views.AddPlaylistView.as_view(), name='add_playlist'),
     path("<slug:slug>/comment", login_required(views.CommentCreateView.as_view()), name="season_comment"),
